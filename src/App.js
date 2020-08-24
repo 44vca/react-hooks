6cd080e82b0667035438069d8951dbb4c7bfeec5
Tonight Dark <aleksandr.abdurazakov@gmail.com>
2020-08-24 23:01:38 +0300

    work on the useRef hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
 
+  const [value, setValue] = useState('initial');
+
+  const renderCount = useRef(1);
+  const inputRef = useRef(null);
+  const prevValue = useRef('');
+
+  useEffect(() => {
+    renderCount.current++;
+  })
+
+  useEffect(() => {
+    prevValue.current = value;
+  }, [value])
+
+  console.log(prevValue.current);
+  console.log(value);
+
+
   return (
     <div>
-      hello
-      <div>world</div>
+      <h1>Count of renders: {renderCount.current}</h1>
+      <h2>Previous value: {prevValue.current}</h2>
+      <input ref={inputRef} type='text' onChange={e => setValue(e.target.value)} value={value} />
     </div>
   );
 }
