c4586d47ef3721dab3a5e7e2f744d1870e15cd50
Tonight Dark <aleksandr.abdurazakov@gmail.com>
2020-08-27 17:02:22 +0300

    practicing useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,103 @@
-import React, { useState } from 'react';
-import Main from './Main';
-import Alert from './Alert';
+import React, { Fragment, useState, useEffect, useReducer } from 'react';
+import axios from 'axios';
+
+const useDataFetch = (initialURL, initialData) => {
+
+  const dataFetchReducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_INIT':
+        return {
+          ...state,
+          isLoading: true,
+        };
+      case 'FETCH_SUCCESS':
+        return {
+          ...state,
+          isLoading: false,
+          data: action.payload,
+        };
+      case 'FETCH_ERROR':
+        return {
+          ...state,
+          isError: true,
+        };
+      default:
+        throw new Error();
+    }
+  }
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  })
+
+  const [url, setUrl] = useState(initialURL);
+
+
+  useEffect(() => {
+    let didCancel = false;
+
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+
+      try {
+        const result = await axios(url);
+
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        }
+      } catch (isError) {
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_ERROR' });
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+
+  return [state, setUrl];
+}
 
 
 function App() {
 
-  const [showAlert, setShowAlert] = useState(false);
+  const [query, setQuery] = useState('redux');
+  const [{ data, isLoading, isError }, doFetch] = useDataFetch(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+    { hits: [] },
+  );
 
-  const toggleShowAlert = () => {
-    setShowAlert(prev => !prev);
-    setTimeout(() => {
-      setShowAlert(prev => !prev);
-    }, 3000)
-  }
-
-  
   return (
-    <div className='container pt-3'>
-      {showAlert && 
-        <Alert />
-        }      
-      <Main showAlert={toggleShowAlert}/>
-    </div>
+    <Fragment>
+      <form onSubmit={event => {
+        doFetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
+
+        event.preventDefault();
+      }}>
+        <input type='text' value={query} onChange={event => setQuery(event.target.value)} />
+        <button type='submit'>Search</button>
+      </form>
+
+      {isError && <div>Something went wrong...</div>}
+
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+          <ul>
+            {data.hits.map(item => (
+              <li key={item.objectID}>
+                <a href={item.url} target='_blank' rel='noopener norefferer'>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        )}
+    </Fragment>
   );
 }
 
